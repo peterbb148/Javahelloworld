104be31043749bc5638fb16e05927200bae17316
Peter Birkholm-Buch <peterbb@gmail.com>
2022-10-05 11:51:29 +0000

    Changed class name

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
-class Simple{  
+class HelloWorld{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello World from Java");  
     }  
 }  
